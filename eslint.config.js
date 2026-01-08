a83811f884ff5a2c2e364a2138af3622364efc6d
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-08 17:17:30 -0600

    Chore: Add rule indent error

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,9 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'indent': ["error", 2],
+      'react/jsx-indent': ['error', 2],
+      'react/jsx-indent-props': ['error', 2],
     },
   },
 ])
