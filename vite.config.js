805d012059236aec79843ef216bda99f2976c41e
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-09 11:17:16 -0600

    Fix: Resolve redirection module alias

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, '/src'),
-      '@root': path.resolve(__dirname, '/'),
+      '@src': path.resolve(__dirname, './src'),
+      '@root': path.resolve(__dirname, './'),
     }
   }
 })
