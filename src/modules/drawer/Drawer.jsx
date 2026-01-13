b029c52ec75e13ec0fea0bad5d8d0d49fa1cb9f8
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-12 23:33:53 -0600

    Feat: Add functional drawer

---
+++ src/modules/drawer/Drawer.jsx
@@ -0,0 +1,111 @@
+import DrawerLayout from "@mui/material/Drawer"
+import List from "@mui/material/List"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import ReceiptLong from '@mui/icons-material/ReceiptLong';
+import Logout from '@mui/icons-material/Logout';
+import { useState } from "react"
+import { fontWeight } from "@root/app-style"
+import DarkMode from '@mui/icons-material/DarkMode';
+import Typography from "@mui/material/Typography"
+import Stack from "@mui/material/Stack"
+import { useNavigate } from "react-router"
+
+const drawerItems = [
+  {
+    id: 1,
+    icon: <DarkMode />,
+    text: "Inventario",
+    path: "inventory"
+  },
+  {
+    id: 2,
+    icon: <ReceiptLong />,
+    text: "Reporte",
+    path: "inventory-report"
+  }
+]
+
+function Drawer({ open, onClose }) {
+  const [selected, setSelected] = useState(drawerItems[0].text)
+  
+  const navigate = useNavigate()
+
+  const handleClick = (item) => {
+    setSelected(item.text)
+    navigate(item.path)
+  }
+
+  return (
+    <DrawerLayout open={open} onClose={onClose}
+      variant="temporary"
+      anchor="right"
+      sx={{
+        ".MuiDrawer-paper": {
+          bgcolor: "primary.main",
+          paddingX: 2
+        }
+      }}
+    >
+      <List
+        sx={{
+          flexDirection: "column",
+          flex: 1,
+          marginTop: 5
+        }}
+      >
+        <Stack sx={{ gap: 1 }}>
+          { drawerItems.map(item => (
+            <DrawerElement
+              key={item.id}
+              title={item.text}
+              icon={item.icon}
+              isSelected={item.text === selected}
+              onClick={() => handleClick(item)}
+            />
+          )) }
+        </Stack>
+      </List>
+      <List>
+        <DrawerElement
+          icon={<Logout />}
+          title="Cerrar Sesión"
+        />
+      </List>
+    </DrawerLayout>
+  )
+}
+
+function DrawerElement({
+  icon, title, isSelected, onClick
+}) {
+  return (
+    <ListItemButton 
+      selected={isSelected}
+      onClick={onClick}
+      sx={{
+        borderRadius: 4,
+        "&.Mui-selected": {
+          bgcolor: "accent.main"
+        }
+      }}
+    >
+      <ListItemIcon sx={{ color: "onPrimary.main" }}>
+        { icon }
+      </ListItemIcon>
+      <ListItemText>
+        <Typography variant="body1"
+          sx={{
+            fontWeight: fontWeight.semibold,
+            color: "onPrimary.main"
+          }}
+        >
+          { title }
+        </Typography>
+      </ListItemText>
+    </ListItemButton>
+  )
+}
+
+export default Drawer
