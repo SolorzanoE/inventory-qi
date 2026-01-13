38c85b273fe5954f1a7a97c1968ea9db41fd377a
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-13 09:55:04 -0600

    Feat: Add main layout

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,36 @@
+import Menu from '@mui/icons-material/Menu';
+import Footer from "@src/modules/footer/Footer"
+import Header from "@src/modules/header/Header"
+import Drawer from '@src/modules/drawer/Drawer';
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import { Outlet } from 'react-router';
+
+function MainLayout() {
+  const [openDrawer, setOpenDrawer] = useState(false)
+
+  const toggleDrawer = () => setOpenDrawer(!openDrawer)
+
+  return (
+    <>
+      <Drawer
+        open={openDrawer} 
+        onClose={toggleDrawer} 
+      />
+      <Header 
+        actionButtons={[{ icon: <Menu />, onClick: toggleDrawer }]} 
+      />
+      <Container maxWidth="xl"
+        sx={{
+          height: "90dvh",
+          bgcolor: "background.main"
+        }}
+      >
+        <Outlet />
+      </Container>
+      <Footer />
+    </>
+  )
+}
+
+export default MainLayout
