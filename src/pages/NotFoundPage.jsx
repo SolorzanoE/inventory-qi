2009a0cdf19f1aa3f6d5dbc8ce9f7112f0338544
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-12 22:32:38 -0600

    Feat: Add page when not found that

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,44 @@
+import Container from "@mui/material/Container"
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+import { fontWeight } from "@root/app-style"
+import SentimentVeryDissatisfied from '@mui/icons-material/SentimentVeryDissatisfied';
+
+function NotFoundPage() {
+  return (
+    <Container maxWidth="xl"
+      sx={{
+        display: "flex",
+        height: "100dvh",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "background.main"
+      }}
+    >
+      <Stack 
+        sx={{ 
+          alignItems: "center", 
+          textAlign: "center", 
+          color: "onBackground.main" 
+        }}
+      >
+        <SentimentVeryDissatisfied 
+          sx={{
+            fontSize: "12rem"
+          }}
+        />
+        <Typography variant="h1" sx={{ fontWeight: fontWeight.semibold }}> 
+          404 
+        </Typography>
+        <Typography variant="h5" sx={{ fontWeight: fontWeight.bold }}> 
+          Not Found 
+        </Typography>
+        <Typography variant="h6" sx={{ fontWeight: fontWeight.medium }}> 
+          La página solicitada no fue encontrada
+        </Typography>
+      </Stack>
+    </Container>
+  )
+}
+
+export default NotFoundPage
