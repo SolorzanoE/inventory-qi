d776641b9835701d69de6a0e6ca1bd43ee21cbbb
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-17 12:27:05 -0600

    Refactor: Add both the inventory and inventory-report routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,12 @@
 import NotFound from "@src/pages/NotFoundPage"
 import { BrowserRouter, Route, Routes } from "react-router"
 import NotAuthorizedPage from "@src/pages/NotAuthorizedPage"
-import { Suspense } from "react"
+import { lazy, Suspense } from "react"
 import MainLayout from "@src/layouts/MainLayout";
 import LoginPage from "@src/pages/LoginPage";
+import InventoryReportPage from "@src/pages/InventoryReportPage";
+
+const InventoryPage = lazy(() => import("@src/pages/InventoryPage"));
 
 const Router = () => (
   <BrowserRouter basename="inventory-qi">
@@ -11,8 +14,8 @@ const Router = () => (
       <Routes>
         <Route index element={<LoginPage />} />
         <Route path="app" element={<MainLayout />} >
-          {/*<Route path="inventory" element={<InventoryPage />} />
-          <Route path="inventory-report" element={<InventoryReportPage />} />*/}
+          <Route path="inventory" element={<InventoryPage />} />
+          <Route path="inventory-report" element={<InventoryReportPage />} />
         </Route>
         <Route path="not-authorized" element={<NotAuthorizedPage />} />
         <Route path="*" element={<NotFound />} />
