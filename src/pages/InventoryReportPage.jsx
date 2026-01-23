489448654556834340683d7ce696d4197711542b
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-23 09:10:51 -0600

    Refactor: Update visual style and some ui interactions

--- src/pages/InventoryReportPage.jsx
+++ src/pages/InventoryReportPage.jsx
@@ -8,14 +8,24 @@ import Grid from "@mui/material/Grid"
 import SelectComponent from "@src/components/selects/SelectComponent"
 import { useState } from "react"
 import CardComponent from "@src/components/cards/CardComponent"
+import SearchComponent from "@src/components/searches/SearchComponent"
 
 const InventoryReportPage = () => {
+  const dataCompany = useState("")
+
+  // [{}]
+  const [reportData, setReportData] = useState([])
+
   return (
     <Stack sx={{ height: "100%" }} spacing={2}>
-      <Stack direction="row">
-        <Typography 
-          variant="h5" 
-          sx={{ 
+      <Stack direction="row"
+        sx={{
+          alignItems: "center"
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
             flex: 1,
             fontWeight: fontWeight.semibold,
             color: "onBackground.main",
@@ -24,80 +34,77 @@ const InventoryReportPage = () => {
         >
           Reporte de Productos
         </Typography>
-        <ActionInventoryReport 
-          properties={{
-            display: { xs: "none", sm: "flex" },
-            flex: 1
-          }} 
-        />
+        <Button
+          sx={{
+            typography: "body1",
+            fontWeight: fontWeight.medium,
+            textTransform: "none",
+            borderRadius: 2,
+            color: "onPrimary.main",
+            bgcolor: "primary.main",
+            width: { xs: 90, sm: 120}
+          }}
+        >
+          Generar
+        </Button>
       </Stack>
-      <Grid container 
+      <Stack direction={{xs: "column", sm: "row"}} spacing={2}
+        sx={{
+          alignItems: { sm: "center" }
+        }}
+      >
+        <SelectComponent
+          isRequired
+          fieldName="company"
+          stateValue={dataCompany}
+          label="Empresa"
+          options={[{ id: 1, nombre: "hola" }]}
+        />
+        <Stack direction="row"
+          spacing={2}
+          sx={{
+            width: { sm: "500%" },
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <SearchComponent />
+          <IconButton
+            onClick={() => { setReportData(data => [...data, 1]) }}
+            sx={{ p: 0 }}
+          >
+            <AddRounded
+              sx={{
+                fontSize: 38,
+                borderRadius: "50%",
+                color: "onPrimary.main",
+                bgcolor: "primary.main"
+              }}
+            />
+          </IconButton>
+        </Stack>
+      </Stack>
+      <Grid container
         spacing={{ xs: 1, sm: 2, md: 3 }}
         rowSpacing={{ xs: 3, sm: 2, md: 5 }}
         columns={{ xs: 2, sm: 3, md: 4, xl: 6 }}
         sx={{ flex: 1, overflow: "auto", pb: 3 }}
       >
-        { new Array(10).fill(0).map((_, index) => (
+        { reportData.map((data, index) => (
           <Grid size={1}
             key={index}
             sx={{ px: { xs: 1, sm: 1, md: 4 } }}
-          > 
+          >
             <Stack sx={{ alignItems: "center" }}>
-              <CardComponent footer={"hola"} title="hola" description="holass" variant="" />
+              <CardComponent
+                variant=""
+                title="data.nombre_procto" description="data.descripcion_producto"
+                footer={"hola"}
+              />
             </Stack>
           </Grid>
         )) }
       </Grid>
-      <ActionInventoryReport properties={{ display: { xs: "flex", sm: "none" }, pb: 3 }} />
-    </Stack>
-  )
-}
-
-const ActionInventoryReport = ({ properties }) => {
-  const dataCompany = useState("")
-
-  return (
-    <Stack direction="row" 
-      spacing={2}
-      sx={{
-        ...properties,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <SelectComponent
-        isRequired
-        fieldName="company"
-        stateValue={dataCompany}
-        label="Empresa"
-        options={[{ id: 1, nombre: "hola" }]}
-      />
-      <Button
-        sx={{
-          typography: "body1",
-          fontWeight: fontWeight.medium,
-          textTransform: "none",
-          borderRadius: 2,
-          color: "onPrimary.main",
-          bgcolor: "primary.main",
-          maxWidth: 150,
-          minWidth: 130
-        }}
-      >
-        Generar
-      </Button>
-      <IconButton
-        sx={{ p: 0 }}
-      >
-        <AddRounded
-          sx={{
-            fontSize: 38,
-            borderRadius: "50%",
-            color: "onPrimary.main",
-            bgcolor: "primary.main"
-          }}
-        />
-      </IconButton>
     </Stack>
   )
 }
