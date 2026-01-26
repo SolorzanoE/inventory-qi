30fa1ae4b9f805641f78e74fde53e7fc3689a4f2
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-26 17:51:20 -0600

    Refactor: Dinamic get endpoint for any environment

--- endpointConfig.js
+++ endpointConfig.js
@@ -1,23 +1,30 @@
-const domainLocal = "http://localhost:5173/"
+const domainDevelopment = "http://localhost:5173/"
+const domainProduction = "https://mistrustingly-systaltic-kason.ngrok-free.dev/"
 
 const entryPoint = "api/"
 
-const developmentEnviroment = {
-  dev: domainLocal
+const currentMode = import.meta.env.MODE
+
+const domains = {
+  development: domainDevelopment,
+  production: domainProduction
 }
 
-const endpoint = { 
+const endpoints = { 
   API_ENDPOINT_COMPANY_LIST: "inventario/empresas",
-  API_ENDPOINT_WAREHOUSES_BY_COMPANY_LIST: "inventario/almacenes/?",
-  API_ENDPOINT_PRODUCT_EXISTENCE_BY_COMPANY_WAREHOUSES_LIST: "inventario/existencias/?/almacen/?",
+  API_ENDPOINT_WAREHOUSE_BY_COMPANY_LIST: "inventario/almacenes/?",
+  API_ENDPOINT_PRODUCT_EXISTENCE_BY_COMPANY_WAREHOUSE_LIST: "inventario/existencias/?/almacen/?",
   API_ENDPOINT_SEARCH_PRODUCT_BY_COMPANY_LIST: "inventario/buscar-producto/?",
   API_ENDPOINT_PRODUCT_EXISTENCE_BY_COMPANY_LIST: "inventario/existencias/?",
-  API_ENDPOINT_SEARCH_PRODUCT_BY_COMPANY_WAREHOUSES_LIST: "inventario/buscar-por-almacen/?/?",
+  API_ENDPOINT_SEARCH_PRODUCT_BY_COMPANY_WAREHOUSE_LIST: "inventario/buscar-por-almacen/?/?",
   API_ENDPOINT_REPORT_COMPARATION_LIST: "inventario/reporte-comparacion/?"
 }
 
-const endpointConfig = (endpointName, enviroment) => (
-  developmentEnviroment[enviroment] + entryPoint + endpoint[endpointName]
+const endpoint = Object.fromEntries(
+  Object.entries(endpoints).map(([key, value]) => {
+    const endpoint = domains[currentMode] + entryPoint + value
+    return [key, endpoint]
+  })
 )
 
-export default endpointConfig
+export default endpoint
