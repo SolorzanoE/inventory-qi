9be1758d756793448bb501246b3200e38508fcde
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-27 16:56:55 -0600

    Feat: Add hooks for consume http/post

---
+++ src/hooks/api/post/useReportComparationList.js
@@ -0,0 +1,29 @@
+import endpoint from "@root/endpointConfig"
+import useApiService from "@src/hooks/useApiService"
+import useSnackbar from "@src/hooks/useSnackbar"
+
+const useReportComparationList = () => {
+  const { responseService, behaivorService, requestService } = useApiService()
+
+  const { showMessage } = useSnackbar()
+
+  if (behaivorService.error) {
+    showMessage("Ocurrió un error al ")
+  }
+
+  // body = [{idProducto, cantidad}]
+  const request = async (companyId, body) => {
+    const URL = endpoint.API_ENDPOINT_REPORT_COMPARATION_POST
+      .replace("*", companyId)
+
+    try {
+      requestService(URL, "POST", body)
+    } catch(error) {
+      console.error(error.message)
+    }
+  }
+
+  return { request, behaivorService, responseService }
+}
+
+export default useReportComparationList
