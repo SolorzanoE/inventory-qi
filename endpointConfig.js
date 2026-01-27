9fb72540ec6ec294fe78da75d7e7a71bfeb56c87
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-01-27 16:54:07 -0600

    Refactor: Add another character for replace in the endpoint

--- endpointConfig.js
+++ endpointConfig.js
@@ -12,12 +12,13 @@ const domains = {
 
 const endpoints = { 
   API_ENDPOINT_COMPANY_LIST: "inventario/empresas",
-  API_ENDPOINT_WAREHOUSE_BY_COMPANY_LIST: "inventario/almacenes/?",
-  API_ENDPOINT_PRODUCT_EXISTENCE_BY_COMPANY_WAREHOUSE_LIST: "inventario/existencias/?/almacen/?",
-  API_ENDPOINT_SEARCH_PRODUCT_BY_COMPANY_LIST: "inventario/buscar-producto/?",
-  API_ENDPOINT_PRODUCT_EXISTENCE_BY_COMPANY_LIST: "inventario/existencias/?",
-  API_ENDPOINT_SEARCH_PRODUCT_BY_COMPANY_WAREHOUSE_LIST: "inventario/buscar-por-almacen/?/?",
-  API_ENDPOINT_REPORT_COMPARATION_LIST: "inventario/reporte-comparacion/?"
+  API_ENDPOINT_WAREHOUSE_BY_COMPANY_LIST: "inventario/almacenes/*",
+  API_ENDPOINT_PRODUCT_EXISTENCE_BY_COMPANY_WAREHOUSE_LIST: "inventario/existencias/*/almacen/*",
+  API_ENDPOINT_SEARCH_PRODUCT_BY_COMPANY_LIST: "inventario/buscar-producto/*?filtro=*",
+  API_ENDPOINT_PRODUCT_EXISTENCE_BY_COMPANY_LIST: "inventario/existencias/*",
+  API_ENDPOINT_SEARCH_PRODUCT_BY_COMPANY_WAREHOUSE_LIST: "inventario/buscar-por-almacen/*/*",
+
+  API_ENDPOINT_REPORT_COMPARATION_POST: "inventario/reporte-comparacion/*"
 }
 
 const endpoint = Object.fromEntries(
