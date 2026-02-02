b6892cb0ee8239a024fe2aaaa6acb941af3a6605
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-02-01 18:38:52 -0600

    Feat: Add more functionality to InventoryPage
    1. Consume some API for any resources
    2. Add controling logic elements for a condition
    3. Adapt elements and can be more easy for the users
    4. Best UI/UX

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -1,17 +1,97 @@
-import Grid from "@mui/material/Grid"
 import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
 import CardComponent from "@src/components/cards/CardComponent"
 import SearchComponent from "@src/components/searches/SearchComponent"
 import SelectComponent from "@src/components/selects/SelectComponent"
-import { useState } from "react"
+import useCompanyList from "@src/hooks/api/get/useCompanyList"
+import useProductExistenceByCompanyList from "@src/hooks/api/get/useProductExistenceByCompanyList"
+import useProductExistenceByCompanyWarehouseList from "@src/hooks/api/get/useProductExistenceByCompanyWarehouseList"
+import useSearchProductByCompanyList from "@src/hooks/api/get/useSearchProductByCompanyList"
+import useSearchProductByCompanyWarehouseList from "@src/hooks/api/get/useSearchProductByCompanyWarehouseList"
+import useWarehouseByCompanyList from "@src/hooks/api/get/useWarehouseByCompanyList"
+import useSnackbar from "@src/hooks/useSnackbar"
+import { selectedCompanyEmptyMessage } from "@src/utils/messages"
+import { useEffect, useState } from "react"
+import SavedSearch from '@mui/icons-material/SavedSearch';
+import Skeleton from "@mui/material/Skeleton"
+import GridCardLayout, { GridCardElement } from "@src/layouts/GridCardLayout"
 
 const InventoryPage = () => {
-  const filterModel = {
+  const { request: companyRequest, responseService: companyResponse } = useCompanyList()
+
+  const { request: warehouseRequest, responseService: warehouseResponse } = useWarehouseByCompanyList()
+
+  const { request: searchProductByCompany, behaivorService: behaivorSearchProductByCompany } = useSearchProductByCompanyList()
+
+  const { request: searchProductByCompanyWarehouse, behaivorService: behaivorSearchProductByCompanyWarehouse } = useSearchProductByCompanyWarehouseList()
+
+  const { request: productExistenceByCompany, behaivorService: behaivorProductExistenceByCompany } = useProductExistenceByCompanyList()
+
+  const { request: productExistenceByCompanyWarehouse, behaivorService: behaivorProductExistenceByCompanyWarehouse } = useProductExistenceByCompanyWarehouseList()
+
+  const { showMessage } = useSnackbar()
+
+  // [{ nombre, descripcion, existencia, ultimoCosto }]
+  const [products, setProducts] = useState([])
+
+  const [searchValue, setSearchValue] = useState("")
+
+  const [selectedData, setSelectedData] = useState({
     company: "",
     warehouse: ""
+  })
+
+  const option = {
+    companies: companyResponse ?? [],
+    warehouses: warehouseResponse ?? []
   }
 
-  const filterData = useState(filterModel)
+  const isSearchingProducts = behaivorSearchProductByCompany.loading || behaivorSearchProductByCompanyWarehouse.loading || behaivorProductExistenceByCompany.loading || behaivorProductExistenceByCompanyWarehouse.loading
+
+  useEffect(() => { companyRequest() }, [])
+
+  useEffect(() => {
+    const load = async () => {    
+      warehouseRequest(selectedData.company)
+      const response = await productExistenceByCompany(selectedData.company)
+      setProducts(response ?? [])
+    }
+
+    if (selectedData.company)
+      load()
+  }, [selectedData.company])
+
+  useEffect(() => {
+    const load = async () => {
+      const response = await productExistenceByCompanyWarehouse(selectedData.company, selectedData.warehouse)   
+      setProducts(response ?? [])
+    }
+
+    if (selectedData.warehouse)
+      load()
+  }, [selectedData.warehouse])
+
+  const handleEnter = async (value) => {
+    if (value === "") 
+      return
+
+    let response = null
+
+    if (!selectedData.company) {
+      showMessage(selectedCompanyEmptyMessage, "warning")
+      return
+    }
+
+    if (!selectedData.warehouse) {
+      response = await searchProductByCompany(selectedData.company, value)
+    } else {
+      response = await searchProductByCompanyWarehouse(selectedData.company, selectedData.warehouse, value)
+    }
+
+    setProducts(response ?? [])
+
+    setSearchValue("")
+  }
 
   return (
     <Stack spacing={2} sx={{ height: "100%" }}>
@@ -30,41 +110,62 @@ const InventoryPage = () => {
           }}
         >
           <SelectComponent 
-            isRequired
             fieldName="company"
-            stateValue={filterData}
+            stateValue={[selectedData, setSelectedData]}
             label="Empresa"
-            options={[{ id: 1, nombre: "hola" }]}
+            options={option.companies}
           />
           <SelectComponent 
+            disabled={!selectedData.company}
             fieldName="warehouse"
-            stateValue={filterData}
+            stateValue={[selectedData, setSelectedData]}
             label="Almácen"
-            options={[{ id: 1, nombre: "hola" }]}
+            options={option.warehouses}
           />
         </Stack>
-        <SearchComponent />
+        <SearchComponent 
+          stateValue={[searchValue, setSearchValue]} 
+          onEnter={handleEnter}
+        />
       </Stack>
-      <Grid container 
-        spacing={{ xs: 1, sm: 2, md: 3 }}
-        rowSpacing={{ xs: 3, sm: 2, md: 5 }}
-        columns={{ xs: 2, sm: 3, md: 4, xl: 6 }}
-        sx={{ flex: 1, overflow: "auto", pb: 3 }}
-      >
-        { new Array(10).fill(0).map((_, index) => (
-          <Grid size={1}
-            key={index}
-            sx={{ px: { xs: 1, sm: 1, md: 4 } }}
-          > 
-            <Stack sx={{ alignItems: "center" }}>
-              <CardComponent footer={"hola"} title="hola" description="hola" variant="" />
-            </Stack>
-          </Grid>
-        )) }
-      </Grid>
+      <GridCardLayout>
+        { (products.length === 0 && !isSearchingProducts) && 
+          <Stack sx={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <SavedSearch sx={{ fontSize: 100 }} color="disabled"/>
+            <Typography variant="h6" sx={{ color: "disabled.main", textAlign: "center" }}> 
+              Empieza buscando productos 
+            </Typography>
+          </Stack>
+        }
+        { isSearchingProducts ? 
+          new Array(10).fill(0).map((_, index) => (
+            <GridCardElement key={index}> 
+              <Skeleton variant="rectangular" 
+                sx={{
+                  width: "100%",
+                  maxWidth: 300,
+                  height: 300,
+                  borderRadius: 4
+                }}
+              /> 
+            </GridCardElement>
+          )) :
+          products.map((element, index) => (
+            <GridCardElement key={index}> 
+              <Stack sx={{ alignItems: "center" }}>
+                <CardComponent 
+                  variant={element.existencia !== 0 ? "normal" : "danger"}
+                  title={element.nombre} 
+                  description={element.description}
+                  footer={`Existencia: ${element.existencia}`} 
+                />
+              </Stack>
+            </GridCardElement>
+          ))
+        }
+      </GridCardLayout>
     </Stack>
   )
 }
-
 
 export default InventoryPage
