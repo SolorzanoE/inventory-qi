cfbf834b1adbeb6de9defdbe8a0197eddfa07afd
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-02-03 17:18:29 -0600

    Feat: Add new message

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -2,6 +2,8 @@ const selectedCompanyEmptyMessage = "Seleccione una empresa para continuar..."
 
 const negativeValueMessage = "No se permiten valores negativos"
 
+const productNotFoundMessage = "No se encontró el producto"
+
 export { 
-  selectedCompanyEmptyMessage, negativeValueMessage
+  selectedCompanyEmptyMessage, negativeValueMessage, productNotFoundMessage
 }
