a9194fed4b0041f54e9ab6b91ccd606e9cc65539
Solórzano <eduardo.solorzano.dev@gmail.com>
2026-02-13 20:08:49 -0600

    Feat: Add base path for the app

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'node:path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/inventory-qi/",
   resolve: {
     alias: {
       '@src': path.resolve('./src'),
